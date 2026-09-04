2c4bc2f9ac0623c2bbc6ce9063ccc79f0387ee19
Martin Fiori <mnf@jphlions.com>
2026-09-04 10:22:00 -0300

    FIX: la url no está hardcodeada

--- src/context/SecurityContext.tsx
+++ src/context/SecurityContext.tsx
@@ -54,7 +54,7 @@ export function SecurityProvider({ children }: SecurityProviderProps) {
   }, []);
 
   const login = useCallback(async () => {
-    const redirectTo = `${process.env.PUBLIC_URL}/auth/callback`;
+    const redirectTo = `/auth/callback`;
 
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
