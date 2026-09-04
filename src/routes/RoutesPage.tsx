0bb3235f632c47a66420bb164c37c1833d64c189
Martin Fiori <mnf@jphlions.com>
2026-09-04 11:44:51 -0300

    CHORE: se eliminó un import sin uso

--- src/routes/RoutesPage.tsx
+++ src/routes/RoutesPage.tsx
@@ -1,5 +1,5 @@
 import { lazy } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import PublicLayout from "../layouts/PublicLayout";
 const AuthCallback = lazy(() => import("../pages/AuthCallback/AuthCallback"));
 const NotFound = lazy(() => import("../pages/NotFound/NotFound"));
